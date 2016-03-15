788b62837e15e363b2e6dfa00491a96ded89c7e4
Mark Tezza <marktezza@gmail.com>
2016-03-15 12:53:12 +0200

    added new project 'map_test'

---
+++ MTMapTest/MTMapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTMapTest
+//
+//  Created by Mark Tezza on 15/03/16.
+//  Copyright © 2016 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
